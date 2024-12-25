820fa2f515b5b87ee11ba3ec7fb9f0abcf15de7e
nariman079 <nariman079i@mail.ru>
2024-12-25 17:53:27 +0300

    fix dist files

--- dist/_next/static/chunks/app/layout-7b2cf4c487b2b40c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4041:(e,s,r)=>{Promise.resolve().then(r.t.bind(r,7970,23)),Promise.resolve().then(r.t.bind(r,347,23)),Promise.resolve().then(r.t.bind(r,5836,23))},347:()=>{},5836:e=>{e.exports={preloader:"style_preloader__sx5gQ"}}},e=>{var s=s=>e(e.s=s);e.O(0,[402,970,441,517,358],()=>s(4041)),_N_E=e.O()}]);
